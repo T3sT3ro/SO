c8f47553cc1db6d1b6bc14c86e484bedbdad61c0
Tooster <max@polarczyk.pl>
2018-11-20 17:59:21 +0100

    Add lab1/zad5.c segfault handling

--- lab1/zad5.c
+++ lab1/zad5.c
@@ -7,7 +7,9 @@
  *  - a w szczególności nie będę go publikować w sieci Internet.
  *
  * Q: Czemu procedura printf nie jest wielobieżna, a snprintf jest?
- * A: ... 
+ * A: snprintf pisze do bufora podanego przez użytkownika. O ile zadbamy o osobny
+ * bufor dla wątków, nie będzie wyścigów. Printf pisze do jednego bufora: stdout, więc
+ * może wystąpić wyścig.
  */
 
 #define _GNU_SOURCE
@@ -19,6 +21,38 @@
 #include <ucontext.h>
 #include <unistd.h>
 
-int main(void) {
-  return EXIT_SUCCESS;
+static void sighandler(int signum, siginfo_t *info, void *_context) {
+    ucontext_t *context = (ucontext_t *)_context;
+    flockfile(stderr);
+    fprintf(stderr, "[%p]<RIP:%llx><RSP:%llx> :\n\t%s\n", info->si_addr,
+             context->uc_mcontext.gregs[REG_RIP], context->uc_mcontext.gregs[REG_RSP],
+             info->si_code == SEGV_MAPERR
+                 ? "Address not mapped to object."
+                 : info->si_code == SEGV_ACCERR ? "Invalid permissions for mapped object."
+                                                : "Welp, SEGFAULT returned other error.");
+    flockfile(stderr);    
+    void *buffer[512];
+    const int calls = backtrace(buffer, sizeof(buffer) / sizeof(void *));
+    backtrace_symbols_fd(buffer, calls, 1);
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
+    struct sigaction sa;
+    sa.sa_sigaction = sighandler;   // handler assign
+    sa.sa_flags = SA_SIGINFO;       // pass context and info to handler
+    sigaction(SIGSEGV, &sa, NULL);  // handler register
+    if (argc != 2 ||
+        (strcmp(argv[1], "--maperr") != 0 && strcmp(argv[1], "--accerr") != 0)) {
+        fprintf(stderr, "Run with either --maperr or --accerr parameter.\n");
+        return EXIT_FAILURE;
+    }
+
+    if (strcmp(argv[1], "--maperr") == 0) {
+        int *ptr = (int *)1;
+        *ptr = 3;
+    } else {
+        int *ptr = (int *)&main;
+        *ptr = 17;
+    }
 }
