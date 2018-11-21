07f50f6252e01ae0fdccce38c661798baccd936c
Tooster <max@polarczyk.pl>
2018-11-21 01:53:53 +0100

    Update lab1/zad6 beautiful character filter

--- lab1/strcnt.c
+++ lab1/strcnt.c
@@ -1,3 +1,9 @@
-int strcnt(const char *str, const char *set) {
-  return 0;
+// assume string and set null terminated
+#include <stdlib.h>
+#include <string.h>
+int strcnt(char* const str, const char* const set) {
+    int writtr = 0;
+    for (int seekr = 0; seekr <= strlen(str); str[writtr++] = str[seekr++])
+        while (seekr < strlen(str) && strchr(set, str[seekr]) != NULL) seekr++;
+    return writtr - 1;
 }
