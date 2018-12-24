7fece74259f7d37d84f26f4debafea007db4b607
Tooster <max@polarczyk.pl>
2018-12-24 02:37:56 +0100

    Fix prog2/zad11

--- prog_2/zad11.c
+++ prog_2/zad11.c
@@ -9,46 +9,37 @@
 #include <assert.h>
 
 #define CARTS 5
-#define DELAYSCALE 100000  // standard delayscale for delay
+#define DELAYSCALE 1000000  // standard delayscale for delay
 #define min(a, b) ((a) < (b) ? (a) : (b))
 pthread_t p_village_producer[2], p_village_consumer[2], p_trolley_operator;
 // each village has one package terminal for produced items and bell to wait for parcel
-sem_t parcel_terminal[2], parcel_bell[2];
-sem_t trolley_unloaded;
+sem_t parcel_inpost[2], parcel_outpost[2], parcel_bell[2];
+sem_t parcel_pending;  // to signal that some village produced something. Either 0 or 1
 // proof of concept - payload items and "queue" for produced items as integer numbers
-int payload = 0, parcel_queue[2] = {0, 0};
-sem_t parcel_pending;  // to signal that some village produced something
+int parcel_inpost_queue[2] = {0, 0}, parcel_outpost_queue[2] = {0, 0};
 
 inline void delay(int timescale) { usleep(timescale + rand() % timescale); }
 
 // not thread-safe. vID_offset=-1 for message without vID
-inline void id_send_msg(int vID, int vID_offset, char* const msg) {
-    if (vID_offset >= 0) msg[vID_offset] = vID + '0';
+inline void send_msg(char* msg) {
     if (write(STDERR_FILENO, msg, strlen(msg)) < 0) exit(EXIT_FAILURE);
-}
-
-// thread-safe (for two village producers)
-void parcel_send_item(int vID) {  // AKA produce and send a single item
-    char _parcel[] = "village _: submitted item to the parcel post.\n";
-    id_send_msg(vID, 8, _parcel);
-    parcel_queue[vID]++;
-    sem_post(&parcel_pending);
-}
-
-// not thread-safe (for two village consumers)
-inline void trolley_unload_item(int vID) {  // pop from queue of items
-    char _unloaded[] = "village _: unloaded item.\n";
-    id_send_msg(vID, 8, _unloaded);
-    payload--;  // take from trolley payload
 }
 
 void* village_producer_sender_lifecycle(void* data) {
     int vID = (int)(long)(data);
+    char _submit[34];
     while (1) {
-        sem_wait(&parcel_terminal[vID]);
-        for (int i = 0; i < 1 + rand() % CARTS; i++) // to see the max 5 payload effect
-            parcel_send_item(vID);
-        sem_post(&parcel_terminal[vID]);
+        sem_wait(&parcel_outpost[vID]);  // exclusive access to outpost
+        int lim = (rand() % 99) + 1;
+        sem_post(&parcel_pending);
+
+        parcel_outpost_queue[vID] += lim;
+        snprintf(_submit, sizeof(_submit),
+                 "village %d: submit --> %2d items.\n",
+                 vID, lim);
+        send_msg(_submit);
+
+        sem_post(&parcel_outpost[vID]);
         delay(DELAYSCALE);  // random sleep for next item generation
     }
 }
@@ -56,53 +47,75 @@ void* village_producer_sender_lifecycle(void* data) {
 // could be split into village_producer/vilage_consumer, but I simply prioritize consuming
 void* village_receiver_consumer_lifecycle(void* data) {
     int vID = (int)(long)(data);
+    char _receive[46];
     while (1) {
-        sem_wait(&parcel_bell[vID]);  // wait till the parcel arrives
+        sem_wait(&parcel_bell[vID]);    // wait till the parcel arrives
+        sem_wait(&parcel_inpost[vID]);  // lock the inbox for exclusive access
+        if (parcel_inpost_queue[vID] > 0) {
+            snprintf(_receive, sizeof(_receive),
+                     "village %d: <-- took %4d items from inpost.\n",
+                     vID, parcel_inpost_queue[vID]);
+            send_msg(_receive);
 
-        assert(payload >= 0 && payload <= 5);
-        while (payload > 0) trolley_unload_item(vID);
-
-        sem_post(&trolley_unloaded);
-        delay(DELAYSCALE / 10);  // unloading 10x faster than production for safety:memory
+            parcel_inpost_queue[vID] = 0;
+        }
+        sem_post(&parcel_inpost[vID]);
+        delay(DELAYSCALE / 10);  // make receive 10x faster
     }
 }
 
 // trolley waits for parcel to arrive, and starting from the opposite of last_sender
 // makes a one way trip
 void* trolley_operator_lifecycle(void* data) {
-    static int last_sender = 0;
-    char _payload[] = "trolley:_ items in payload. sending to _\n";
+    int last_sender = 0;
+    int payload = 0;
+    char _transfer[46];
     while (1) {
-        sem_wait(&trolley_unloaded);  // wait untill trolley is free
-        sem_wait(&parcel_pending);    // sleep until any parcel comes
+        sem_wait(&parcel_pending);  // wait for parcel
+        // if in here, one of outposts has queue > 0
 
-        sem_wait(&parcel_terminal[1 - last_sender]);  // start with other than last sender
-        if (parcel_queue[1 - last_sender] > 0) {      // iff there are items to be sent
-            payload = min(parcel_queue[1 - last_sender], CARTS);  // don't overload
-            parcel_queue[1 - last_sender] -= payload;
-        }
-        sem_post(&parcel_terminal[1 - last_sender]);
+        sem_wait(&parcel_outpost[1 - last_sender]);        // starvation prevention
+        if (parcel_outpost_queue[1 - last_sender] == 0) {  // if current favoured empty
+            last_sender = 1 - last_sender;                 // swap priority
+            sem_post(&parcel_outpost[last_sender]);
+        } else
+            sem_post(&parcel_outpost[1 - last_sender]);
 
-        if (payload > 0) {  // if we have a parcel to deliver from this village
-            _payload[39] = '0' + last_sender;
-            id_send_msg(payload, 8, _payload);
-            delay(DELAYSCALE);                        // make a trip
-            sem_post(&parcel_bell[last_sender]);  // signal delivery complete
-            last_sender = 1 - last_sender;  // swap the priority (starvation prevention)
-        } else {  // otherwise check if we have a parcel from the other village
+        int vID_from = 1 - last_sender, vID_to = last_sender;
+
+        sem_wait(&parcel_outpost[vID_from]);  // locking the outposts
+        payload = min(parcel_outpost_queue[vID_from], CARTS);
+        parcel_outpost_queue[vID_from] -= payload;
+        if (parcel_outpost_queue[vID_from] > 0)
             sem_post(&parcel_pending);
-            sem_post(&trolley_unloaded);
-        }
+        sem_post(&parcel_outpost[vID_from]);
+        //if (payload == 0) continue;
+
+        delay(DELAYSCALE / 60);  // transfer time
+        snprintf(_transfer, sizeof(_transfer),
+                 "\x1B[34;12m[transfer]: %d --[%2d]--> %d\x1B[0m\n",
+                 vID_from, payload, vID_to);
+        send_msg(_transfer);
+
+        sem_wait(&parcel_inpost[vID_to]);  // lock the inpost
+        parcel_inpost_queue[vID_to] += payload;
+        sem_post(&parcel_inpost[vID_to]);
+        sem_post(&parcel_bell[vID_to]);  // signal parcel delivery
+
+        payload = 0;
+        last_sender = 1 - last_sender;  // swap the priority
     }
 }
 
+// to stop the program, ^C
 int main() {
-    sem_init(&trolley_unloaded, 1);  // trolley is unloaded at first
-    sem_init(&parcel_pending, 0);    // no pending parcels at first
+    sem_init(&parcel_pending, 0);  // no pending parcels at first
 
+    pthread_create(&p_trolley_operator, NULL, &trolley_operator_lifecycle, NULL);
     for (long i = 0; i < 2; i++) {
-        sem_init(&parcel_terminal[i], 1);  // terminals for parcels are open
-        sem_init(&parcel_bell[i], 0);      // nothing to unload at first
+        sem_init(&parcel_inpost[i], 1);   // terminals for parcels are open
+        sem_init(&parcel_outpost[i], 1);  // ^^^
+        sem_init(&parcel_bell[i], 0);     // nothing to receive at first
 
         int err = 0;
         if ((err = pthread_create(&p_village_consumer[i], NULL,
@@ -112,7 +125,7 @@ int main() {
                                   &village_producer_sender_lifecycle, (void*)(long)i)))
             exit(err);
     }
-    pthread_create(&p_trolley_operator, NULL, &trolley_operator_lifecycle, NULL);
-    pthread_join(p_trolley_operator, NULL);  // end with ^C
-    return 0;  // sigmask etc. would be the same as in the 2nd task
+
+    pthread_join(p_trolley_operator, NULL);
+    return 0;
 }
